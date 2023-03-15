7f4dd141fcbf4b3f74f0c5b9e14bd2d7473c7a98
unknown <jcf.daw@gmail.com>
2023-03-16 00:44:15 +0100

    finished demo

--- index.js
+++ index.js
@@ -28,28 +28,45 @@ const megaTeacher = {
         }
 
     },
-    modifyAddress: function(calle, numero, piso, add) {
+    modifyAddress: function(newStreet, newNumber, newFloor, addSecond) {
 
         // Solución ejercicio 5:
         // return `La nueva dirección es ${megaTeacher.address.street = calle}, ${megaTeacher.address.number = numero}. Piso ${megaTeacher.address.floor = piso}`;
 
         // Ejercicio 6:
-        // if((megaTeacher.address.street === null || megaTeacher.address.street === '' || megaTeacher.address.street === ' ') || (megaTeacher.address.number === null || megaTeacher.address.number === '' || megaTeacher.address.number === ' ') || (megaTeacher.address.floor === null || megaTeacher.address.floor === '' || megaTeacher.address.floor === ' ')) {
-        //     return `La dirección es ${megaTeacher.address.street = calle}, ${megaTeacher.address.number = numero}. Piso ${megaTeacher.address.floor = piso}`;
-        // } else {
-        //     return `La nueva dirección es ${megaTeacher.address.street = calle}, ${megaTeacher.address.number = numero}. Piso ${megaTeacher.address.floor = piso}`;
-        // }
 
-        if(add) {
-            megaTeacher.address2 = {
-                street: calle,
-                number: numero,
-                floor: piso
+        if(addSecond === true) {
+            const address2 = {
+                street: newStreet,
+                number: newNumber,
+                floor: newFloor
+            }
+            if(this.address2) {
+                return this.address2 = address2;
+            } else {
+                return this.address2 = address2;
             }
         } else {
-            return `La nueva dirección es ${megaTeacher.address.street = calle}, ${megaTeacher.address.number = numero}. Piso ${megaTeacher.address.floor = piso}`;
+            this.address.street = newStreet;
+            this.address.number = newNumber;
+            this.address.floor = newFloor;
+            return `La dirección es ${newStreet}, ${newNumber}. Piso ${newFloor}`;
         }
+    },
+    verifyBook: function(book) {
 
+        const arrBook = megaTeacher.books;
+
+        // Ejercicio 7:
+        // arrBook.includes(book) ? console.log(`El libro '${book}' está en la lista de llibros.`) : console.log(`El libro '${book}' no existe en la lista.`);
+
+        // Ejercicio 8:
+        if(arrBook.includes(book)) {
+            return console.log('Libro existente en la lista: ', book);
+        } else {
+            arrBook.push(book);
+            return console.log(`Libro añadido: ${book}. La lista contiene: ${arrBook.join(', ')}`)
+        }
     }
 }
 
@@ -139,8 +156,52 @@ console.log('==============================================');
 console.log(megaTeacher.modifyAddress('Bilbao', 18, 8, true));
 console.log(megaTeacher);
 
+console.log(megaTeacher.modifyAddress('SantaCruz', 32, 2, true));
+console.log(megaTeacher);
+
+console.log('==============================================');
 
 
-// Escribe un método que en el array de libros, verifique si un libro existe en el array.
-// Si existe, imprima el libro en mayusculas. Si no existe agregar a la lista de libros.
-// Escribe una función que te permita añadir películas. Y si no tiene parámetro de entrada te retorne la película de en medio.
+// 7- Escribe un método que en el array de libros, verifique si un libro existe en el array.
+
+megaTeacher.verifyBook('El alquimista'); // Prueba verifyBook
+megaTeacher.verifyBook('Lord of the Flies');
+
+console.log('==============================================');
+
+// 8- Si existe, imprima el libro en mayusculas. Si no existe agregar a la lista de libros.
+megaTeacher.verifyBook('The perfume');
+console.log(megaTeacher.books); // Prueba de libros añadidos con verifyBook
+
+console.log('==============================================');
+
+// 9- Escribe una función que te permita añadir películas. Y si no tiene parámetro de entrada te retorne la película de en medio.
+function addFilm(film) {
+    const arrFilms = megaTeacher.films;
+
+    if(film) {
+        arrFilms.push(film);
+        return `Película añadida: ${film}. La lista contiene: ${arrFilms.join(', ')}`
+    } else {
+        if(arrFilms.length % 2 === 0) {
+            return 'Hay un número de películas pares. No hay medio exacto'
+        } else {
+            return megaTeacher.films[(megaTeacher.films.length - 1) / 2];
+        }
+    }
+}
+
+
+console.log(addFilm('Big Fish'));
+
+console.log(megaTeacher.films)
+
+console.log(addFilm());
+
+console.log(addFilm('The Exorcist'));
+
+console.log(addFilm()); // Peli intermedia
+
+
+
+console.log(megaTeacher);
